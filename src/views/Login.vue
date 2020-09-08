68465e48f421464ef8fc3d949e02fa1d6b398caa
Davi <davicamiloribeiro@gmail.com>
2020-09-08 19:04:52 -0300

    Tela de login e autenticação

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="home">
-        <h1>Login</h1>
-        <hr>
-        <div class="columns is-centered">
-            <div class="column is-half">
-                <div v-if="erro != undefined">
-                    <div class="notification is-danger">
-                        <p>{{erro}}</p>
+    <div class="wrapper">
+        <div class="box">
+            <h1>Login</h1>           
+            <div class="boxIntern">
+                <div class="column">
+                    <div v-if="erro != undefined">
+                        <div class="notification is-danger">
+                            <button class="delete" @click="close"></button>
+                            <p>{{erro}}</p>
+                        </div>
                     </div>
+                    <input type="email" placeholder="E-mail" v-model="email">
+                    <input type="password" placeholder="Senha" v-model="password">
+                    
+                    <button class="button is-success is-outlined is-rounded" @click="login">Logar</button>
                 </div>
-                <p>E-mail</p>
-                <input type="email" placeholder="email@email.com" class="input" v-model="email">
-                <p>Senha</p>
-                <input type="password" placeholder="*****" class="input" v-model="password">
-                <hr>
-                <button class="button is-success" @click="login">Logar</button>
             </div>
-        </div>
-    </div> 
+        </div> 
+    </div>
 </template>
 
 <script>
@@ -43,11 +43,93 @@ export default {
                 var msgErro = err.response.data.err;
                 this.erro = msgErro;
             })
+        },
+        close(){
+            this.erro = undefined;
         }
     }
 }
 </script>
 
 <style scoped>
+.wrapper{
+	height: 100vh;
+	width: 100vw;
+	background-color: #34495e;
+	position: absolute;
+	left: 0;
+	top: 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #fff;
+	transition: .5s;
+}
+.box{
+    width: 350px;
+    height: 400px;
+    padding: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background: rgba(0,0,0,0.5);
+    text-align: center;
+}
+.boxIntern{
+    padding-top: 50px;
+}
+.box h1{
+  color: white;
+  text-transform: uppercase;
+  font-weight: 500;
+}
+.box input[type = "email"],.box input[type = "password"]{
+  border:0;
+  background: none;
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+  border: 2px solid #3498db;
+  padding: 14px 10px;
+  width: 200px;
+  outline: none;
+  color: white;
+  border-radius: 24px;
+  transition: 0.25s;
+}
+.box input[type = "email"]:focus,.box input[type = "password"]:focus{
+  width: 280px;
+  border-color: #2ecc71;
+}
+.box .button{
+  width: 200px;
+  border:0;
+  background: none;
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+  border: 2px solid #2ecc71;
+  outline: none;
+  color: white;
+  border-radius: 24px;
+  transition: 0.25s;
+  cursor: pointer;
+}
+::-webkit-input-placeholder {
+   color: grey;
+}
+
+:-moz-placeholder { /* Firefox 18- */
+   color: grey;
+}
+
+::-moz-placeholder {  /* Firefox 19+ */
+   color: grey;
+}
+
+:-ms-input-placeholder {  
+   color: grey;  
+}
 
 </style>
